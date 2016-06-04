7066eee3f2dae98ebd9b58698ba0ce698e113b9e
PendragonDevelopment <me@jordankburke.com>
2016-06-04 12:24:01 -0400

    Added provisional ACC member to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+puts "Adding Athens Clarke commissioners"
+RepresentativeProfile.create(name: "Sharyn Dickerson",
+ office: "Commissioner",
+ district: "1",
+ term: "Mon, 31 Dec 2018 00:00:00 EST -05:00",
+ term_start: "Thu, 01 Jan 2015 00:00:00 EST -05:00",
+ bio:
+  "- President, Sharyn Dickerson & Associates, LLC (2005-present)\r\n- Assistant Solid Waste Director, Unified Government of Athens-Clarke County (1997-2005, 1994-1996)\r\n- Interim Solid Waste Director, Unified Government of Athens-Clarke County (1996-1997)\r\n- Recycling Coordinator, Unified Government of Athens-Clarke County (1991-1994)\r\n- Inside Sales Representative, Scientific Atlanta (1990-1991)",
+ profession: "President, Sharyn Dickerson & Associates, LLC (2005-present)",
+ party_affiliation: "",
+ official_email: "sharyn.dickerson@athensclarkecounty.com")

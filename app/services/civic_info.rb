ece036f1e7f06046f3489e2c3b1861e742cc6244
dmcbrayer <dmcbrayer@gmail.com>
2016-06-04 13:35:30 -0400

    The CivicInfo class now works, basically

--- app/services/civic_info.rb
+++ app/services/civic_info.rb
@@ -5,58 +5,38 @@ class CivicInfo
   # various government districts (Congressional District,
   # County Commission District, etc.)
 
+  include HTTParty
+  base_uri 'https://www.googleapis.com/civicinfo/v2'
+
   def initialize(address)
     @address = address
     @api_key = "AIzaSyCBry-wWNlRyWlE-i9a4Oj8Kbxyvd7wW8o"
+    set_options
   end
 
-  def get_civic_info
-    # get the info from Google
-    {
-      "kind": "civicinfo#representativeInfoResponse",
-      "normalizedInput": {
-        "line1": "6105 rushing parc ln",
-        "city": "birmingham",
-        "state": "AL",
-        "zip": "35244"
-      },
-      "divisions": {
-        "ocd-division/country:us/state:al/county:shelby/council_district:5": {
-          "name": "Shelby County Commission District 5"
-        },
-        "ocd-division/country:us/state:al/cd:6": {
-          "name": "Alabama's 6th congressional district"
-        },
-        "ocd-division/country:us/state:al/place:hoover": {
-          "name": "Hoover city"
-        },
-        "ocd-division/country:us": {
-          "name": "United States"
-        },
-        "ocd-division/country:us/state:al/sldu:14": {
-          "name": "Alabama State Senate district 14"
-        },
-        "ocd-division/country:us/state:al/county:shelby": {
-          "name": "Shelby County"
-        },
-        "ocd-division/country:us/state:al/sldl:15": {
-          "name": "Alabama State House district 15"
-        },
-        "ocd-division/country:us/state:al": {
-          "name": "Alabama"
-        }
+  def set_options
+    @options = {
+      query: {
+        address: @address,
+        includeOffices: 'true',
+        key: @api_key
       }
     }
   end
 
+  def get_civic_info
+    # get the info from Google
+    self.class.get('/representatives', @options)
+  end
+
   def get_commission_district
-    council_key = get_civic_info[:divisions].keys.select { |k| k.to_s.include? "council_district" }
+    council_key = get_civic_info["divisions"].keys.select { |k| k.to_s.include? "council_district" }
     council_key.first.to_s.last.to_i
   end
 
   # Not necessary right now, but may be useful
   def get_divisions
-    get_civic_info[:divisions].map { |d| d[1][:name] }
+    get_civic_info["division"].map { |d| d[1]["name"] }
   end
 
 end

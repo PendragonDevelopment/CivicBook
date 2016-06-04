3495e8718ab76b36cf262a44f2333d40dbc415bc
PendragonDevelopment <me@jordankburke.com>
2016-06-04 13:28:38 -0400

    Added avatar to rep profile

--- app/models/representative_profile.rb
+++ app/models/representative_profile.rb
@@ -1,2 +1,11 @@
 class RepresentativeProfile < ActiveRecord::Base
+
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604155238) do
+ActiveRecord::Schema.define(version: 20160604172707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 20160604155238) do
     t.string   "profession"
     t.string   "party_affiliation"
     t.string   "official_email"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|

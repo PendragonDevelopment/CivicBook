5bbe6d558b485b9f5b50d11ffd7a312d244dc4fe
PendragonDevelopment <me@jordankburke.com>
2016-06-04 16:44:17 -0400

    Hash gymnastics

--- app/controllers/civic_info_controller.rb
+++ app/controllers/civic_info_controller.rb
@@ -3,16 +3,18 @@ class CivicInfoController < ApplicationController
 
   def get_representatives
     @address = params[:address]
-    @ci = CivicInfo.new(@address)
+    @ci = CivicInfo::Base.new(@address)
     @district = @ci.commission_district
-    @info_hash = @ci.info_hash
+    @officials = @ci.officials
+    @info_hash = @ci.concise_info
 
-    redirect_to representatives_results_path(district: @district, info_hash: @info_hash)
+    redirect_to representatives_results_path(district: @district, info_hash: @info_hash, officials: @officials)
   end
 
   def representatives_results
     @district = params[:district]
     @info_hash = params[:info_hash]
+    @officials = params[:officials]
   end
 
 end

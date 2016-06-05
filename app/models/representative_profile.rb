fac41b90d7b18a41cab3319ca6de15a9f83d59be
PendragonDevelopment <me@jordankburke.com>
2016-06-05 11:19:37 -0400

    Removed Layout 3 from git and added default images

--- app/models/representative_profile.rb
+++ app/models/representative_profile.rb
@@ -26,7 +26,7 @@ class RepresentativeProfile < ActiveRecord::Base
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  }, :default_url => ":style/missing.png"
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
